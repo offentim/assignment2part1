d0f003fdcc8d22abd4eb914abae776413bf2dc43
Tim Offen <offentim@gmail.com>
2019-08-15 14:51:24 +1200

    Working accelerometer

--- app/src/main/java/com/example/assignment2part1/MainActivity.java
+++ app/src/main/java/com/example/assignment2part1/MainActivity.java
@@ -1,16 +1,113 @@
 package com.example.assignment2part1;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
+
+
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+
+
+
+    String test;
+
+    private static SensorManager sensorManager;
+    private Sensor sensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (sensor != null){
+            sensorManager.registerListener(this,sensor,sensorManager.SENSOR_DELAY_NORMAL);
+
+        }else{
+            Toast.makeText(this, "Not Supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+
+        String textX = String.format(x+"");
+        textView.setText(textX);;
+
+
+        String textY = String.format(y+"");
+        textView2.setText(textY);
+
+        String textZ = String.format(z+"");
+        textView3.setText(textZ);
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
-
-//Josh Branch 1 
